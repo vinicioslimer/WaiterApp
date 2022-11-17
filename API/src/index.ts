b2cd2c1f386c7242b1f7b610ccdcbde3045cbb97
Vinicios Limer <vinicioslimer@outlook.com>
2022-11-16 22:34:00 -0300

    Implementando banco de dados

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const app = express();
 
-app.listen(3001, () => {
-  console.log("Server listening on port 3001");
-});
+
+mongoose.connect('mongodb:http://localhost:27017')
+.then(() => {
+  const app = express();
+  app.listen(3001, () => {
+    console.log("Server listening on port 3001");
+  });
+})
+.catch(() => console.log('Erro ao conectar ao mongodb'))
+
+
